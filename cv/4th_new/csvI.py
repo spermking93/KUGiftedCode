4556b2e2cab28284b6d83e9fea7e84a8c3bc38d4
ujos89 <48623147+ujos89@users.noreply.github.com>
2020-08-07 13:57:37 +0900

    Update csvI.py

--- cv/4th_new/csvI.py
+++ cv/4th_new/csvI.py
@@ -4,4 +4,5 @@ import numpy as np
 data = list(range(100))
 print(data)
 
+#txt를 저장하는 함수(delimiter는 구분자, header는 문서첫줄에 들어갈 머리말, comments는 머리말에 처음 들어가는 단어 default는 '#'
 np.savetxt("outTest.csv", data, delimiter=",",header="num",comments='')
